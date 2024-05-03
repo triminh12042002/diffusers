9b071c17c600b455bec4c26a7c7850e0a00992e1
triminh12042002 <triminh12042002@gmail.com>
2024-05-03 15:04:02 +0700

    chore: lint

--- examples/text_to_image/train_mgd.py
+++ examples/text_to_image/train_mgd.py
@@ -204,7 +204,6 @@ def main():
     # unet = torch.hub.load(map_location=device, dataset=args.dataset, repo_or_dir='/home/tri/Uni/Year4/Thesis/Experiment/results/diffusers/train_output/unet/', source='local',
     #                       model='diffusion_pytorch_model.safetensors', pretrained=False)
 
-    # unet = UNet2DConditionModel.from_pretrained("/home/tri/Uni/Year4/Thesis/Experiment/results/diffusers/train_output/unet/",use_safetensors=True)
     
     # unet = UNet2DConditionModel.from_pretrained("teslayder/mgd",use_safetensors=True)
 
@@ -307,111 +306,6 @@ def main():
         eps=args.adam_epsilon,
     )
 
-    # Get the datasets: you can either provide your own training and evaluation files (see below)
-    # or specify a Dataset from the hub (the dataset will be downloaded automatically from the datasets Hub).
-
-    # In distributed training, the load_dataset function guarantees that only one local process can concurrently
-    # download the dataset.
-    # if args.dataset_name is not None:
-    #     # Downloading and loading a dataset from the hub.
-    #     dataset = load_dataset(
-    #         args.dataset_name,
-    #         args.dataset_config_name,
-    #         cache_dir=args.cache_dir,
-    #         data_dir=args.train_data_dir,
-    #     )
-    # else:
-    #     data_files = {}
-    #     if args.train_data_dir is not None:
-    #         data_files["train"] = os.path.join(args.train_data_dir, "**")
-    #     dataset = load_dataset(
-    #         "imagefolder",
-    #         data_files=data_files,
-    #         cache_dir=args.cache_dir,
-    #     )
-    #     # See more about loading custom images at
-    #     # https://huggingface.co/docs/datasets/v2.4.0/en/image_load#imagefolder
-
-    # # Preprocessing the datasets.
-    # # We need to tokenize inputs and targets.
-    # column_names = dataset["train"].column_names
-
-    # # 6. Get the column names for input/target.
-    # dataset_columns = DATASET_NAME_MAPPING.get(args.dataset_name, None)
-    # if args.image_column is None:
-    #     image_column = dataset_columns[0] if dataset_columns is not None else column_names[0]
-    # else:
-    #     image_column = args.image_column
-    #     if image_column not in column_names:
-    #         raise ValueError(
-    #             f"--image_column' value '{args.image_column}' needs to be one of: {', '.join(column_names)}"
-    #         )
-    # if args.caption_column is None:
-    #     caption_column = dataset_columns[1] if dataset_columns is not None else column_names[1]
-    # else:
-    #     caption_column = args.caption_column
-    #     if caption_column not in column_names:
-    #         raise ValueError(
-    #             f"--caption_column' value '{args.caption_column}' needs to be one of: {', '.join(column_names)}"
-    #         )
-
-    # # Preprocessing the datasets.
-    # # We need to tokenize input captions and transform the images.
-    # def tokenize_captions(examples, is_train=True):
-    #     captions = []
-    #     for caption in examples[caption_column]:
-    #         if isinstance(caption, str):
-    #             captions.append(caption)
-    #         elif isinstance(caption, (list, np.ndarray)):
-    #             # take a random caption if there are multiple
-    #             captions.append(random.choice(caption) if is_train else caption[0])
-    #         else:
-    #             raise ValueError(
-    #                 f"Caption column `{caption_column}` should contain either strings or lists of strings."
-    #             )
-    #     inputs = tokenizer(
-    #         captions, max_length=tokenizer.model_max_length, padding="max_length", truncation=True, return_tensors="pt"
-    #     )
-    #     return inputs.input_ids
-
-    # # Preprocessing the datasets.
-    # train_transforms = transforms.Compose(
-    #     [
-    #         transforms.Resize(args.resolution, interpolation=transforms.InterpolationMode.BILINEAR),
-    #         transforms.CenterCrop(args.resolution) if args.center_crop else transforms.RandomCrop(args.resolution),
-    #         transforms.RandomHorizontalFlip() if args.random_flip else transforms.Lambda(lambda x: x),
-    #         transforms.ToTensor(),
-    #         transforms.Normalize([0.5], [0.5]),
-    #     ]
-    # )
-
-    # def preprocess_train(examples):
-    #     images = [image.convert("RGB") for image in examples[image_column]]
-    #     examples["pixel_values"] = [train_transforms(image) for image in images]
-    #     examples["input_ids"] = tokenize_captions(examples)
-    #     return examples
-
-    # with accelerator.main_process_first():
-    #     if args.max_train_samples is not None:
-    #         dataset["train"] = dataset["train"].shuffle(seed=args.seed).select(range(args.max_train_samples))
-    #     # Set the training transforms
-    #     train_dataset = dataset["train"].with_transform(preprocess_train)
-
-    # def collate_fn(examples):
-    #     pixel_values = torch.stack([example["pixel_values"] for example in examples])
-    #     pixel_values = pixel_values.to(memory_format=torch.contiguous_format).float()
-    #     input_ids = torch.stack([example["input_ids"] for example in examples])
-    #     return {"pixel_values": pixel_values, "input_ids": input_ids}
-
-    # DataLoaders creation:
-    # train_dataloader = torch.utils.data.DataLoader(
-    #     train_dataset,
-    #     shuffle=True,
-    #     collate_fn=collate_fn,
-    #     batch_size=args.train_batch_size,
-    #     num_workers=args.dataloader_num_workers,
-    # )
-
     train_dataset = VitonHDDataset(
         dataroot_path=args.dataset_path,
         phase='train',
@@ -423,7 +317,7 @@ def main():
 
     train_dataloader = torch.utils.data.DataLoader(
         train_dataset,
-        shuffle=False,
+        shuffle=True,
         batch_size=args.batch_size,
         num_workers=args.num_workers_test,
     )
@@ -671,23 +565,6 @@ def main():
         # aligning device to prevent device errors when concating it with the latent model input
         masked_image_latents = masked_image_latents.to(device=device, dtype=dtype)
         return mask, masked_image_latents
-    
-    # def _execution_device():
-    #     r"""
-    #     Returns the device on which the pipeline's models will be executed. After calling
-    #     `pipeline.enable_sequential_cpu_offload()` the execution device can only be inferred from Accelerate's module
-    #     hooks.
-    #     """
-    #     if device != torch.device("meta") or not hasattr(unet, "_hf_hook"):
-    #         return device
-    #     for module in unet.modules():
-    #         if (
-    #                 hasattr(module, "_hf_hook")
-    #                 and hasattr(module._hf_hook, "execution_device")
-    #                 and module._hf_hook.execution_device is not None
-    #         ):
-    #             return torch.device(module._hf_hook.execution_device)
-    #     return device
     
 
     # Train!
